58f2525289ab64f88150aaf839170af427a7346a
AmroBaniIssa <amro.nedal.issa@gmail.com>
2023-08-15 06:42:00 +0000

    .

---
+++ src/components/footer/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// class Footer extends React.Component {
+//   render() {
+//     return <footer>&copy; 2018</footer>;
+//   }
+// }
+
+// export default Footer;
+
+
+function Footer(props) {
+      return (
+          <>
+         <footer>&copy; 2018</footer>
+       </>
+      )
+  
+  }
+  export default Footer;
